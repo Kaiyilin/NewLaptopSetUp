989e39ea5b77c68f78b3f8328ad6b8c276fe8c47
kaiyiling3 <kaiyiling3@gmail.com>
2024-05-16 03:46:20 +0000

    add selection menu

--- setup_python_with_conda.sh
+++ setup_python_with_conda.sh
@@ -23,22 +23,28 @@ rm -rf ~/miniconda3/miniconda.sh
 # the input of $SHELLBASE should be zsh or bash
 # and init the conda
 SHELLBASE=$(basename "$SHELL")
+echo "Init conda...."
 ~/miniconda3/bin/conda init $SHELLBASE 
 
 # pick a python version
 py_version=(
-    "3.6"
-    "3.7"
-    "3.8"
     "3.9"
+    "3.8"
+    "3.7"
+    "3.6"
 )
 
-default_py_version_index=3
+# create a conda environment
+default_py_version_index=0
 read -p "your conda env name?" condaEnvName
-read -p "your python env version?m (default: ${py_version[$default_py_version_index]}) [${py_version[@]}): " PythonEnv
+PS3="your python env version? (default: ${py_version[$default_py_version_index]})"
+
+select PythonEnv in "${py_version[@]}"; do
+    break
+done
 
 # export PATH=~/miniconda3/bin:$PATH
-conda -n $condaEnvName python=$PythonEnv
+conda create -n $condaEnvName python=$PythonEnv
 conda activate $condaEnvName
 
 
